37756cb2299035b66589439a4cf3e14954f7e538
Administrator <Administrator@SCSBWIN-667643>
2016-08-12 03:58:07 -0700

    asdad

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ bot.dialog("/", [
 ]);
 
 */
-
+/*
 var builder = require("botbuilder");
 var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
@@ -111,3 +111,19 @@ bot.dialog('/', [
 ]);
 
 
+*/
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Create bot and add dialogs
+var bot = new builder.BotConnectorBot({ appId: '3cc73cc0-e212-488e-8d47-4cab7f593c32', appSecret: '6VG4DophXbiNuTdKdBEWQSn' });
+bot.add('/', function (session) {
+    session.send('Hello World');
+});
+
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
