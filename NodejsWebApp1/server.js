bbfe058351dfbab082802503d8c64f261dd174f2
Administrator <Administrator@SCSBWIN-667643>
2016-08-11 22:55:50 -0700

    commit

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -4,14 +4,12 @@ var bot = new builder.UniversalBot(connector);
 var intents = new builder.IntentDialog();
 bot.dialog('/', intents);
 intents.matches(/^Hi/i, [
-    function(session)  
-   {  
-                builder.Prompts.text(session, 'Hi there! How are you today?');  
-            },
-       function(session, results)  
-   {  
-                session.send('%s! How can I help you?', results.response);  
-            }  
+    function (session) {
+        builder.Prompts.text(session, 'Hi there! How are you today?');
+    },
+    function (session, results) {
+        session.send('%s! How can I help you?', results.response);
+    }
 ]);
 intents.matches(/^need version/i, builder.DialogAction.send('The Latest Bot version is 1.1'));
 intents.onDefault(builder.DialogAction.send('Hi there! How can I help you today?'));
