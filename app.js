0456a8f596667a68b1cee1c0d5a3e49bb3c96cb0
Administrator <Administrator@SCSBWIN-667643>
2016-08-12 03:52:47 -0700

    new file

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
-var restify = require('restify');
+﻿var restify = require('restify');
 var builder = require('botbuilder');
 
-//=========================================================
-// Bot Setup
-//=========================================================
+// Create bot and add dialogs
+var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
+bot.add('/', function (session) {
+    session.send('Hello World');
+});
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
-});
-  
-// Create chat bot
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
-var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
-
-//=========================================================
-// Bots Dialogs
-//=========================================================
-
-bot.dialog('/', function (session) {
-    session.send("Hello World");
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
 });
