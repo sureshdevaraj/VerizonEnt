2f7924ee561c0f77f82fe8a607aa56db9d6e0d45
Administrator <Administrator@SCSBWIN-667643>
2016-08-16 07:14:58 -0700

    new files

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-﻿# NodejsWebApp1
+﻿# VerizonEnt
 
 
---
+++ server.ts
@@ -0,0 +1,15 @@
+﻿var restify = require('restify');
+var builder = require('botbuilder');
+
+// Create bot and add dialogs
+var bot = new builder.BotConnectorBot({ appId: '3cc73cc0-e212-488e-8d47-4cab7f593c32', appSecret: '6VG4DophXbiNuTdKdBEWQSn' });
+bot.add('/', function (session) {
+    session.send('Hello World');
+});
+
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
